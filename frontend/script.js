279c09f322c8f1fea248d39389e9388747e2f30a
ElohegyiI <roxnadz78@gmail.com>
2022-03-03 17:16:52 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,34 +1,77 @@
 
 
 
-function init(mathFunction) {
-    var  a = 5;
-    var b  = 6;
-}
+function loadEvent() {
+
+    const rootElement = document.getElementById('root')
+
+    const division = document.createElement('div')
+        division.className = 'form'
+        document.getElementById('root').appendChild(division)
 
 
+        const formHTML = () => { 
+            return `
+            <form>
+            
+            <input id="name1" name="input1" type="text"><br>
+            
+            <input id="name2" name="input2" type="text"><br>
+            
+            <input id="name3" name="input3" type="text"><br>
+            <select name="animals" id="animals">
+                <option value="cats5555">cats</option>
+                <option value="both5555">Both</option>
+                <option value="dogs">dogs</option>                
+            </select>
+            <button>Click</button>
 
-if ( a < b ) {
-
-    let c = mathFunction(b, a)
-
-    /*(function() {
-        var c = b - a;*/
-    } else {
-
-        let c = mathFunction(a, b)
-
-        /*(function(){
-            var c = a - b;*/
+            </form>`           
 
         }
 
-const initC = (firstNumber, secondNumber) => {}
+        
+        
+        rootElement.insertAdjacentHTML('beforeend', formHTML())
+          
+        const form = rootElement.querySelector('form')
 
-    return firstNumber - secondNumber
 
-const initD = (firstNumber, secondNumber) => {}
+        const inputList = document.queryselectorAll('input')
 
-    return firstNumber * secondNumber
-     
-init(initD)
+        console.log(typeof inputList);
+        console.log(Array.isArray(inputList));
+
+        /*for (const input of inputList ) {
+
+            input.addEventListener('input', function(event) {
+
+                console.log(event.target.value)
+
+            }
+        } */
+
+        Array.from(inputList).map(function(input) {
+            input.addEventListener('input', function(event) {
+
+            console.log(event.target.value);
+
+        }) 
+        
+    });
+        
+        form.querySelector('select').addEventListener('input', function(event) {
+            console.log(event.target);
+
+        })
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault()
+             console.log(event.target)
+        })    
+
+
+
+}
+
+window.addEventListener('load', loadEvent)
