e338cd7613757a6d2d2566a6b6d8628b3bc5cfae
ElohegyiI <roxnadz78@gmail.com>
2022-03-04 19:18:38 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,78 +1,82 @@
 
 
 
+const formHTML = () => { 
+    return `
+    <form>    
+        <input id="input1" name="input1" type="text"><br>
+
+        <input id="input2" name="input2" type="text"><br>
+
+        <input id="input3" name="input3" type="text"><br>
+        
+        <select name="animals" id="animals">
+        <option value="cats5555">cats</option>
+        <option value="both5555">Both</option>
+        <option value="dogs">dogs</option>                
+        </select>
+        <button>Send</button>
+    </form>`           
+
+}
+
 const loadEvent = () => {
 
     const rootElement = document.getElementById('root')
 
-    const division = document.createElement('div')
+    /*const division = document.createElement('div')
         division.className = 'form'
-        document.getElementById('root').appendChild(division)
+        document.getElementById('root').appendChild(division)*/
 
 
-        const formHTML = () => { 
-            return `
-            <form>
-            
-            <input id="name1" name="input1" type="text"><br>
-            
-            <input id="name2" name="input2" type="text"><br>
-            
-            <input id="name3" name="input3" type="text"><br>
-            <select name="animals" id="animals">
-                <option value="cats5555">cats</option>
-                <option value="both5555">Both</option>
-                <option value="dogs">dogs</option>                
-            </select>
-            <button>Click</button>
 
-            </form>`           
-
-        }
-
-        
-        
         rootElement.insertAdjacentHTML('beforeend', formHTML())
-          
+        
         const form = rootElement.querySelector('form')
-
+        
+          
 
         const inputList = document.queryselectorAll('input')
 
-        console.log(typeof inputList);
-        console.log(Array.isArray(inputList));
+        //console.log(typeof inputList);
+        //console.log(Array.isArray(inputList));
 
+        
+        Array.from(inputList).map(function(input) {
+            input.addEventListener('input', function(event) {
+                
+                console.log(event.target.value);
+                
+            })
+            
+        });
+        
+        /*rootElement.querySelectorAll('form').addEventListener('submit'), function(event) {
+            event.preventDefault() 
+        }
+        form.querySelector('select').addEventListener('input', function(event) {
+            console.log(event.target);
+            
+        })*/
+
+        form.querySelector('select').addEventListener('input', function(event) {
+        
+        form.addEventListener('submit', function(event) {
+            event.preventDefault()
+            console.log(event.target)
+        })  
+        }
         /*for (const input of inputList ) {
 
             input.addEventListener('input', function(event) {
 
                 console.log(event.target.value)
 
-            }
-        } */
-
-        Array.from(inputList).map(function(input) {
-            input.addEventListener('input', function(event) {
-
-            console.log(event.target.value);
-
-        }) 
+            })
+        }*/
+        /*Fetch dátumokra, több képre*/
         
-    });
-        
-        form.querySelector('select').addEventListener('input', function(event) {
-            console.log(event.target);
-
-        })
-
-        form.addEventListener('submit', function(event) {
-            event.preventDefault()
-             console.log(event.target)
-        })    
-
-/*Fetch dátumokra, több képre*/
-
-    const nasaApiKey = 'd8rG7d4dYIisXPSU8KBH17ipMYR7pPGj1tjRgNqD'
+    /*const nasaApiKey = 'd8rG7d4dYIisXPSU8KBH17ipMYR7pPGj1tjRgNqD'
     const requestedDate = "2022.02.22"
     const apod = await fetch(".....${apiKey}....${requestedDate")
 
@@ -90,9 +94,9 @@ const loadEvent = () => {
             ))
         }
     )
-    `)
-
-
+    `)*/
 }
+
+
 
 window.addEventListener('load', loadEvent)
