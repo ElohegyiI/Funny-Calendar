e852d0bc1ff4c45f4d1e11e4206d86d8eea86027
ElohegyiI <roxnadz78@gmail.com>
2022-03-15 06:13:37 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -185,15 +185,33 @@ getUserIdentifiers(users)*/
         gender: "female",
         age: 17,
         isConfirmed: false
+      },
+      {
+        id: 4733632,
+        username: "akarkine99",
+        email: "ifjkazmerne@gmail.com",
+        phone: "206546547",
+        gender: "female",
+        age: 19,
+        isConfirmed: false
+      },
+      {
+        id: 4733632,
+        username: "akarkine99",
+        email: "ifjkazmerne@gmail.com",
+        phone: "206546547",
+        gender: "female",
+        age: 16,
+        isConfirmed: false
       }
   ]
 
 
-function getFemaleAdults(user) {
+function getFemaleAdults(users) {
   
     let x = [];
 
-    for (const names of user) {
+    for (const user of users) {
         
         if( user.gender === 'female' && user.age >= 18 ) {
 
@@ -208,3 +226,262 @@ function getFemaleAdults(user) {
   
 }
 getFemaleAdults(user)*/
+
+/*const item = [
+
+    {
+        name: 'Bike', price: 100, id: 1
+    },
+    {
+        name: 'TV', price: 200, id: 2
+    },
+    {
+        name: 'Book', price: 50, id: 3
+    }
+
+]
+
+function myNew(items) {
+
+    let result = [];
+
+    for (const item of items) {
+
+        if ( item.id === 1 ) {
+
+            result.push(item)
+            console.log(item)
+        }
+        //return result
+    }
+    
+}
+myNew(item)
+
+/*const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split('');
+
+const newWords = words.reverse()
+
+const newNew = newWords.join('')
+
+let result = [];
+
+result.push(newNew)
+
+
+
+console.log(result)*/
+
+
+/*const array = 'hfisjsndze'
+
+const newArray = array.split('')
+
+const newNew = newArray.reverse()
+
+const newJoin = newNew.join('')
+
+function myFunction(params) {
+    
+    return newJoin
+    
+    
+}
+console.log(newJoin)*/
+
+/*function myName(name, lastName) {
+
+    console.log('Hello Faszom' + name + ' ' + lastName)
+}
+myName('juno', 'etyke')
+
+for ( i = 5; i >= 1; i-- ) {
+
+    if ( i % 2 !== 0)
+
+    console.log(i)
+
+}*/
+
+
+
+
+
+const companies = [
+    { name: 'Company One', category: 'finance', start: 1978, end: 2000 },
+    { name: 'Company Two', category: 'retail', start: 1980, end: 2010 },
+    { name: 'Company Three', category: 'auto', start: 1979, end: 2002 },
+    { name: 'Company Four', category: 'technology', start: 1988, end: 2020},
+    { name: 'Company Five', category: 'auto', start: 1998, end: 2001 },
+    { name: 'Company Six', category: 'technology', start: 1999, end: 2015 },
+    { name: 'Company Seven', category: 'finance', start: 1923, end: 2001 },
+    { name: 'Company Eight', category: 'technology', start: 1945, end: 2022 }
+]
+
+const ages = [ 12, 34, 43, 67, 89, 51, 29, 79, 65, 23, 10 ]
+
+/*for (let i = 0; i < companies.length; i++) {
+    console.log(companies[i])
+}*/
+
+/*companies.forEach(function(company) {
+    console.log(company.name)
+})*/
+
+/*let canDrink = [];
+
+for (let i = 0; i < ages.length; i++) {
+    if (ages[i] >= 21 ) {
+        canDrink.push(ages[i])
+    }
+
+}*/
+
+/*const canDrink = ages.filter(function(ages) {
+
+    if( ages >= 21 ) {
+        return true
+    }
+
+})*/
+
+//const canDrink = ages.filter(ages => ages >= 21)
+
+/*const retailCompanies = companies.filter(function(company) {
+
+    if (company.category === 'retail') {
+        return true
+    }
+    
+})*/
+
+/*const retailCompanies = companies.filter(company => company.category === 'retail')
+
+const eigthiesCompanies = companies.filter(company => (company.start >= 1980 && company.end < 2010))
+
+const lastedTenYears = companies.filter(company => (company.end - company.start > 10))
+
+console.log(lastedTenYears)*/
+
+//map
+
+/*const companyNames = companies.map(function(company) {
+
+    return company.name
+
+})*/
+
+/*const testMap = companies.map(function(company) {
+    return `${company.name} [${company.start} - ${company.end}]`
+})*/
+
+/*const testMap = companies.map(company => 
+    `${company.name} [${company.start} - ${company.end}]`
+)*/
+
+//console.log(companyNames)
+
+//const agesSquare = ages.map(age => Math.sqrt(age))
+//const agesTimesTwo = ages.map(age => age * 2)
+
+/*const mapMap = ages
+.map(age => Math.sqrt(age))
+.map(age => age * 2)
+
+console.log(mapMap)*/
+
+
+/*const sortedCompanies = companies.sort(function(c1, c2) {
+
+    if (c1.start > c2.start) {
+        return 1;
+    } else {
+        return -1;
+    }
+    
+});*/
+
+/*const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1))
+
+const sortedAges = ages.sort((a, b) => b - a)
+
+console.log(sortedAges)*/
+
+/*let ageSum = 0;
+
+for (let i = 0; i < ages.length; i++) {
+    ageSum += ages[i]
+}*/
+
+
+/*const ageSum = ages.reduce((total, age) => 
+    total + age
+, 0);*/
+
+
+/*const totalYears = companies.reduce(function(total, company) {
+    return total + (company.end - company.start)
+}, 0);*/
+
+/*const totalYears = companies.reduce((total, company) => 
+    total + (company.end - company.start)
+, 0);*/
+
+/*for (let i = 0; i < companies.length; i++) {
+
+    console.log(companies[i])
+}
+
+companies.forEach(function(company) {
+    console.log(company.name)
+})*/
+
+/*let canDrink = [];
+
+for (let i = 0; i < ages.length; i++) {
+    if ( ages[i] >= 35 ) {
+        canDrink.push(ages[i])
+    }
+}
+console.log(canDrink)*/
+
+/*const canDrink = ages.filter(function(ages) {
+    if(ages >= 35) {
+        return true
+    }
+});*/
+
+/*const companyCategory = companies.filter(function(company) {
+
+    if(company.category === 'retail') {
+        return true
+    }
+    
+})
+
+const lastedTenYears = companies.filter((company)  => {
+
+    if(company.end - company.start > 20) {
+        return true
+    }
+    
+});
+console.log(lastedTenYears)*/
+
+function myFilter(company) {
+
+    for (let i = 0; i < companies.length; i++) {
+
+        if (company.start < 2010) {
+            
+            return true
+            
+        }
+    }
+    console.log(company.start)
+
+    
+}
+myFilter()
